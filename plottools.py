4aed803fc6f4744d2698fcee11c7f8dda6558d5a
Isaac Gresham <igresham27@gmail.com>
2017-11-27 21:57:51 +1100

    Fixed copypaste error

---
+++ plottools.py
@@ -0,0 +1,525 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from refnx._lib import flatten
+import matplotlib.patches as mpatches
+import corner
+import refnx
+import brush
+
+def unpack_objective(obj):
+    data = obj.data
+    structure = obj.model.structure
+    model = obj.model
+    
+    return data, structure, model
+
+
+def plot_logrefl(objective, samples=None, colour = 'k', alpha = 0.05, limits = None):
+    
+    data, structure, model = unpack_objective(objective)
+
+    if np.all(samples) != None:
+        if type(colour) != list:
+            colour = [colour]*len(samples)
+            
+        for i, c in zip(samples, colour):
+            objective.setp(i)
+            plt.plot(data.x, model(data.x, x_err=data.x_err), color=c, alpha = alpha)
+    else:
+        plt.plot(data.x, model(data.x, x_err=data.x_err), color=colour, alpha = alpha)
+            
+    plt.errorbar(data.x, data.y, yerr=data.y_err, marker='.', color='k')
+    plt.xlabel('Q')
+    plt.title("Log Reflectivity")
+    plt.ylabel('logR')
+    plt.yscale('log')
+    
+    if limits != None:
+        plt.xlim(limits[0:2])
+        plt.ylim(limits[2:])
+    
+def plot_rq4refl(objective, samples=None, colour = 'k', alpha = 0.05, limits = None):
+    
+    data, structure, model = unpack_objective(objective)
+    
+    if np.all(samples) != None:
+        for i in samples:
+            objective.setp(i)
+            plt.plot(data.x, model(data.x, x_err=data.x_err)*(data.x**4), color=colour, alpha = alpha)
+    else:
+        plt.plot(data.x, model(data.x, x_err=data.x_err)*(data.x**4), color=colour, alpha = alpha)
+
+    plt.errorbar(data.x, data.y*(data.x**4), yerr=data.y_err*(data.x**4), marker='.', color='k')
+    plt.xlabel('Q')
+    plt.ylabel('RQ^4')
+    plt.yscale('log')
+    plt.title("RQ4 Reflectivity")
+    
+    if limits != None:
+        plt.xlim(limits[0:2])
+        plt.ylim(limits[2:])
+        
+
+def plot_SLD(objective, samples=None, colour = 'k', alpha = 0.05):
+    
+    data, structure, model = unpack_objective(objective)
+    print('hi')
+    if np.all(samples) != None:
+        if type(colour) != list:
+            colour = [colour]*len(samples)
+        for i, c in zip(samples, colour):
+            print (c)
+            objective.setp(i)
+            plt.plot(*structure.sld_profile(), color=c, alpha = alpha)
+    else:
+        plt.plot(*structure.sld_profile(), color=colour, alpha = alpha)
+
+    plt.title("SLD Profile")
+    plt.xlabel('z (Angstrom)')
+    plt.ylabel('SLD')
+    
+
+def fit(objective, full_output=False, method='differential_evolution'):
+    data, structure, model = unpack_objective(objective)
+    print('Before fit:')
+    print("Chi Squared:" , objective.chisqr())
+    plot(data, structure, model)
+    
+    if full_output:
+        print(objective)
+    
+    print("Fitting...")
+    fitter = CurveFitter(objective, threads=4, nwalkers=100)
+    
+    try:
+        fitter.fit(method)
+    except np.linalg.linalg.LinAlgError:
+        print("Errored Out. Finished!")
+        
+    print("After fit:")
+    print("Chi Squared:" , objective.chisqr())
+    plot(data, structure, model)
+    
+    if full_output:
+        print(objective)
+
+
+        
+def plot_walker_trace(objective, fitter, burn=100):
+    chain = fitter.sampler.chain
+    
+    labels = []
+    for i in flatten(objective.parameters.varying_parameters()):
+        labels.append(i.name)
+
+    # Print walker tracks
+    
+    if len(chain.shape) == 3: # Not parallel tempering
+        num_subplot_rows = int(np.ceil(chain.shape[2]/2))
+        plt.figure(figsize=(14,5*num_subplot_rows))
+        for c in range(chain.shape[2]):
+
+            plt.subplot(num_subplot_rows,2,c+1)
+            plt.title(str(c) + ' ' + labels[c])
+            for j in chain:
+                plt.plot(j[:,c], alpha=0.1, color='k')
+            plt.axvline(x=burn, color='red', linestyle='dashed')
+
+    else: #then PT
+        num_subplot_rows = int(np.ceil(chain.shape[3]/2))
+        plt.figure(figsize=(14,5*num_subplot_rows))
+        for c in range(chain.shape[3]):
+
+            plt.subplot(num_subplot_rows,2,c+1)
+            plt.title(str(c) + ' ' + labels[c])
+            for temp in chain:
+                for walker in temp:
+                    plt.plot(walker[:,c], alpha=0.1, color='k')
+                plt.axvline(x=burn, color='red', linestyle='dashed')
+        
+    
+    return plt.gcf()
+
+def plot_profiles(objective, samples=None, colour = 'k', alpha = 0.1):
+    if objective == None:
+        return
+    
+    plt.subplot(2,2,1)
+    plot_SLD(objective, samples, colour = colour, alpha = alpha)
+    plt.subplot(2,2,2)
+    plot_logrefl(objective, samples, colour = colour, alpha = alpha)
+    plt.subplot(2,2,3)
+    plot_rq4refl(objective, samples, colour = colour, alpha = alpha)
+    plt.subplot(2,2,4)
+    plot_logrefl(objective, samples, colour = colour, alpha = alpha, limits=[0,0.05,1e-3,1.1])
+    
+    return plt.gcf()
+
+
+        
+def plot_corner(objective, samples):
+    labels = []
+    for i in flatten(objective.parameters.varying_parameters()):
+        labels.append(i.name)    
+    
+    fig = corner.corner(samples, labels=labels, quantiles=[0.025, 0.5, 0.975],
+                       show_titles=True, title_kwargs={"fontsize": 12})
+        
+    return fig
+        
+        
+def plot_cluster_profiles(db, objective, samples, lnprob = None):
+
+    core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
+    core_samples_mask[db.core_sample_indices_] = True
+    labels = db.labels_
+    n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+    unique_labels = list(reversed(list(set(labels))))
+    colors = [plt.cm.Spectral(each)
+          for each in np.linspace(0, 1, len(unique_labels))]
+
+    plt.figure(figsize=(14,10))
+
+    leg_labels = []
+    c = 0
+    for k, col in zip(unique_labels, colors):
+        print (c , ":" , n_clusters_)
+        c += 1
+        if k == -1:
+            # Black used for noise.
+            col = [0, 0, 0, 1]
+
+        class_member_mask = (labels == k)
+        clustered_samp = samples[class_member_mask]
+        
+        plot_profiles (objective, clustered_samp, tuple(col))  
+        
+        if np.all(lnprob) != None:
+            string = 'max local prob  = %.2f' % np.max(lnprob[class_member_mask])
+            leg_labels.append(mpatches.Patch(color=col, label=string))
+    
+    if np.all(lnprob) != None:
+        high_prob = np.max(lnprob)            
+        high_prob_index =  lnprob == high_prob
+        high_prob_index = np.reshape(high_prob_index, -1)
+        high_prob_sample = samples[high_prob_index, :]
+        
+        plot_profiles (objective, high_prob_sample, colour='red', alpha=1)
+
+        string = 'max global prob = %.2f' % high_prob
+        leg_labels.append(mpatches.Patch(color='red', label=string))
+
+        plt.subplot(2,2,4)
+        plt.legend(handles=leg_labels)
+
+    return plt.gcf()
+
+def process_data(fitter, burn=0, flatten = False, thin=0):
+    """
+    fitter.process_chain returns an inconvienient data structure
+    """
+    if len(fitter.sampler.chain.shape) == 3: # No PT
+        samples = fitter.sampler.chain[:,burn:]
+        lnprob = fitter.sampler.lnprobability[:,burn:]
+        if flatten:
+            samples = samples.reshape(-1, samples.shape[2])
+            samples = samples[0::thin]
+            lnprob = lnprob.reshape(-1, 1)
+            lnprob = lnprob[0::thin]
+    else: # Then PT
+        chain = fitter.sampler.chain
+        chain = chain.reshape((chain.shape[0]*chain.shape[1], chain.shape[2], chain.shape[3]))
+        
+        lnprob = fitter.sampler.lnprobability
+        lnprob = lnprob.reshape((lnprob.shape[0]*lnprob.shape[1], lnprob.shape[2]))
+        
+        samples = chain[:,burn:]
+        lnprob = lnprob[:,burn:]
+        if flatten:
+            samples = samples.reshape(-1, samples.shape[2])
+            samples = samples[0::thin]
+            lnprob = lnprob.reshape(-1, 1)
+            lnprob = lnprob[0::thin]
+    return samples, lnprob
+
+
+def TempSort (objectives, Temps):
+    objectives = [x for _,x in sorted(zip(Temps,objectives))]
+    Temps = sorted(Temps)   
+    Temps = np.array(Temps)
+    return objectives, Temps
+
+def pltpub_SLD(ax, objectives, colours=[], markers=[], z=None, fontsize=14, title='VFP profile'):
+    
+    if markers == []:
+        markers = ['solid']*len(objectives)
+    if colours == []:
+        colours = [plt.cm.set2(each) for each in np.linspace(0, 1, len(objectives))]
+    
+    if np.all(z) == None:
+        z = np.linspace(0,1200, 10000)
+    
+
+    for obj, col, marker in zip (objectives, colours, markers):
+        sld = obj.model.structure.sld_profile()
+        ax.plot(sld[0]/10, sld[1], color=col, linestyle=marker)
+
+    
+    return ax
+    
+def pltpub_VFP(ax, objectives, colours=[], markers=[], z=None, fontsize=14, title='VFP profile'):
+    if markers == []:
+        markers = ['solid']*len(objectives)
+    if colours == []:
+        colours = [plt.cm.set2(each) for each in np.linspace(0, 1, len(objectives))]
+
+    if np.all(z) == None:
+        z = np.linspace(0,1200, 10000)
+    
+    # Areas = []
+
+    for obj, col, marker in zip (objectives, colours, markers):
+        spline = find_FFVFP (obj.model.structure)
+        vfp = spline.volume_fraction_profile(z)
+        mask = vfp > 0
+        ax.plot(z[mask]/10, vfp[mask], color=col, linestyle=marker)
+
+            
+
+            
+    #leg_areas = []
+    #for L, A in zip(leg, areas):
+    #    leg_areas.append(('%s   %.1fnm' % (L, A/10)))
+
+    #ax.legend(leg_areas)
+          
+    #fig = plt.gcf()
+    #plt.show()
+    
+    return ax
+    
+def pltpub_logrefl(ax, objectives, colours=[], markers=[],  z=None,  max_q=0.3, offset=100, show_data=True):
+    if markers == []:
+        markers = ['solid']*len(objectives)
+    if colours == []:
+        colours = [plt.cm.set2(each) for each in np.linspace(0, 1, len(objectives))]
+        
+    
+    scale = 1
+
+    for obj, col, marker in zip (objectives, colours, markers):
+        data, structure, model = unpack_objective(obj)
+        q_mask = data.x < max_q
+        x = data.x[q_mask]
+        y = data.y[q_mask]
+        x_err = data.x_err[q_mask]
+        y_err = data.y_err[q_mask]
+            
+        ax.plot(x, model(x, x_err=x_err)/scale, color=col, linestyle = marker)
+        if show_data:
+            ax.errorbar(x, y/scale, yerr=y_err/scale, marker='.', color=col, alpha=0.25)
+        scale *= offset
+
+
+    ax.set_yscale('log')
+    ax.axes.yaxis.set_ticklabels([])
+    ticks = 10**(-np.linspace(0,np.log10(offset)*len(objectives)+4,2*len(objectives)+5))
+    ax.set_yticks(ticks) 
+
+
+    
+    return ax
+    
+    
+def pltpub_rq4refl(ax, objectives, colours=[], markers=[],  z=None,  max_q=0.3, offset=100, show_data=True):
+    if markers == []:
+        markers = ['solid']*len(objectives)
+    if colours == []:
+        colours = [plt.cm.set2(each) for each in np.linspace(0, 1, len(objectives))]
+        
+    
+    scale = 1
+    
+    for obj, col, marker in zip (objectives, colours, markers):
+        data, structure, model = unpack_objective(obj)
+        q_mask = data.x < max_q
+        x = data.x[q_mask]
+        y = data.y[q_mask]
+        x_err = data.x_err[q_mask]
+        y_err = data.y_err[q_mask]
+            
+        ax.plot(x, model(x, x_err=x_err)*(x**4)/scale, color=col, linestyle = marker)
+        if show_data:
+            ax.errorbar(x, y*(x**4)/scale, yerr=y_err*(x**4)/scale, marker='.', color=col, alpha=0.25)
+        scale *= offset
+
+
+    #plt.xlabel('Scattering Vector, ' + r'$Q$' + r' $(\AA^{-1})$')
+    #plt.ylabel(r'$RQ^4$')
+    ax.set_yscale('log')
+
+    ax.axes.yaxis.set_ticklabels([])
+    ticks = 10**(-np.linspace(7,7+np.log10(offset)*len(objectives)+1,2*len(objectives)+2))
+    ax.set_yticks(ticks) 
+    
+    return ax
+    
+    
+    
+
+def plot_publishable(objectives, labels, z=None, max_q=0.2, Temps=[]):
+    if len(Temps) == 0:
+        colours = [plt.cm.plasma(each)
+          for each in np.linspace(0, 1, len(objectives))]
+    else:
+        assert len(Temps) == len(labels), "you have supplied %d temperatures and %d labels." % (len(Temps), len(labels))
+        
+        objectives = [x for _,x in sorted(zip(Temps,objectives))]
+        Temps = sorted(Temps)   
+        Temps = np.array(Temps)
+        scaledTemps = (Temps-10)/40 #assuming maximum temperature we'll get is 50 and minimum is 10
+        colours = [plt.cm.plasma(each)
+          for each in np.linspace(min(scaledTemps), max(scaledTemps), len(objectives))]
+            
+    if np.all(z) == None:
+        z = np.linspace(0,1200, 10000)
+        
+    marker = 'solid'
+    for obj, col in zip (objectives, colours):
+        sld = obj.model.structure.sld_profile()
+        plt.plot(*sld, color=col, linestyle=marker)
+        if marker == 'solid':
+            marker = 'dashed'
+        else:
+            marker = 'solid'
+    
+    if len(Temps) == 0:
+        LA = []
+        label_length = max_len(labels)
+        plt.legend(labels)
+    else:
+        TA = []
+        for T in Temps:
+            TA.append(('%.1f°C ' % T))
+        fig=plt.gcf()
+        ax=fig.gca()
+        plt.text(0.815, 0.96, '         Area', transform=ax.transAxes)
+        plt.legend(TA,loc='upper right', bbox_to_anchor=(0.999, 0.97))
+        
+    plt.title("SLD Profile")
+    plt.xlabel('z (Angstrom)')
+    plt.ylabel('SLD Profile')
+
+    
+    areas = []
+    marker = 'solid'
+    for obj, col in zip (objectives, colours):
+        spline = find_FFVFP (obj.model.structure)
+        areas.append(spline.profile_area())
+        vfp = spline.volume_fraction_profile(z)
+        mask = vfp > 0
+        plt.plot(z[mask], vfp[mask], color=col, linestyle=marker)
+        if marker == 'solid':
+            marker = 'dashed'
+        else:
+            marker = 'solid'
+    
+    if len(Temps) == 0:
+        LA = []
+        label_length = max_len(labels)
+        for L, A in zip(labels, areas):
+            LA.append((L + ' '*(label_length - len(L)) + ' %d.0 A' % A))
+        plt.legend(LA)
+    else:
+        TA = []
+        for T, A in zip(Temps, areas):
+            print 
+            TA.append(('%.1f°C   %.1fnm' % (T, A/10)))
+        fig=plt.gcf()
+        ax=fig.gca()
+        plt.text(0.815, 0.96, 'Temp     Area', transform=ax.transAxes)
+        plt.legend(TA,loc='upper right', bbox_to_anchor=(0.999, 0.97))
+        
+        
+    plt.title("Volume Fraction Profile")
+    plt.xlabel('z (Angstrom)')
+    plt.ylabel('Volume Fraction Profile')
+        
+    plt.show()
+    
+    plt.figure(figsize=(14, 14))
+    
+
+    plt.subplot(1,2,1)
+    offset = 1
+    marker = 'solid'
+    for obj, col in zip (objectives, colours):
+        data, structure, model = unpack_objective(obj)
+        q_mask = data.x < max_q
+        x = data.x[q_mask]
+        y = data.y[q_mask]
+        x_err = data.x_err[q_mask]
+        y_err = data.y_err[q_mask]
+            
+        plt.plot(x, model(x, x_err=x_err)/offset, color=col, linestyle = marker)
+        plt.errorbar(x, y/offset, yerr=y_err/offset, marker='.', color=col)
+        offset *= 100
+        
+        if marker == 'solid':
+            marker = 'dashed'
+        else:
+            marker = 'solid'
+
+    plt.xlabel('Q')
+    plt.title("Log Reflectivity")
+    plt.ylabel(r'$\mathrm{log}(R)$')
+    plt.yscale('log')
+    plt.gca().axes.yaxis.set_ticklabels([])
+    ticks = 10**(-np.linspace(0,2*len(objectives)+4,2*len(objectives)+5))
+    plt.gca().set_yticks(ticks) 
+    
+    plt.subplot(1,2,2)
+    
+    offset = 1
+    marker = 'solid'
+    for obj, col in zip (objectives, colours):
+        data, structure, model = unpack_objective(obj)
+        q_mask = data.x < max_q
+        x = data.x[q_mask]
+        y = data.y[q_mask]
+        x_err = data.x_err[q_mask]
+        y_err = data.y_err[q_mask]
+
+        plt.plot(x, model(x, x_err=x_err)*(x**4)/offset, color=col, linestyle = marker)
+        plt.errorbar(x, y*(x**4)/offset, yerr=y_err*(x**4)/offset, marker='.', color=col)
+        offset *= 100
+        
+        if marker == 'solid':
+            marker = 'dashed'
+        else:
+            marker = 'solid'
+    
+    plt.xlabel('Q')
+    plt.ylabel(r'$RQ^4$')
+    plt.yscale('log')
+    plt.title(r"$RQ^4$ Reflectivity")
+    plt.gca().axes.yaxis.set_ticklabels([])
+    ticks = 10**(-np.linspace(7,7+2*len(objectives)+1,2*len(objectives)+2))
+    plt.gca().set_yticks(ticks) 
+    
+
+        
+def max_len(L):
+    ml = 0
+    for i in L:
+        if len(i) > ml:
+            ml = len(i)
+    return ml
+        
+def find_FFVFP (struct):
+    for i in struct:
+        if isinstance(i, refnx.reflect.brush.Freeform_dzVFP):
+            return i
+        elif isinstance(i, brush.FreeformVFP):
+            return i
+    return None
