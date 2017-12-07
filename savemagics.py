f26f3b789cbfae7841b4268f70c2323142224d47
Isaac Gresham <igresham27@gmail.com>
2017-12-07 20:02:03 +1100

    update to savemagics to auto write objective wrapper

--- savemagics.py
+++ savemagics.py
@@ -46,3 +46,43 @@ class CellWriter(Magics):
 
     
 get_ipython().register_magics(CellWriter)
+
+
+def writeMPI(objective_name, nwalkers, ntemps, nsteps, nthin, init_method = 'prior', buffering=1000):
+    """
+    """
+    
+    code = "\
+import sys\n\
+from refnx.analysis import CurveFitter\n\
+from emcee.utils import MPIPool\n\
+from %s import *\n\
+objective = setup()\n\
+with MPIPool() as pool:\n\
+    if not pool.is_master():\n\
+        pool.wait()\n\
+        sys.exit(0)\n\
+\n\
+    nwalkers=%d\n\
+    ntemps=%d\n\
+    nsteps=%d\n\
+    nthin=%d\n\
+\n\
+    fitter_dry = CurveFitter(objective, nwalkers=nwalkers, ntemps=ntemps)\n\
+\n\
+    fitter_dry.initialise('%s')\n\
+\n\
+    with open('%s' + '_samplechain_' +\n\
+              str(nwalkers) + 'walkers_' +\n\
+              str(ntemps) + 'temps_' + \n\
+              str(nsteps) + 'steps_' + \n\
+              str(nthin) + 'thinned.txt',\n\
+              'a', buffering=%d) as fh:\n\
+\n\
+        fitter_dry.sample(nsteps, nthin=nthin, pool=pool, verbose=True, f=fh)\
+"%(objective_name, nwalkers, ntemps, nsteps, nthin, init_method, objective_name, buffering)
+        
+    filename = objective_name + "_run.py"
+        
+    with open(filename, 'w') as fh: # Save the cell + metadata as a .py file
+        fh.write(code)
