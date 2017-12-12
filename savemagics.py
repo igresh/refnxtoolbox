1c5fa4c91e7e12625e3935ddc1cc47620b4e89c8
Isaac Gresham <igresham27@gmail.com>
2017-12-13 09:20:16 +1100

    Finished initial development of savemagics - now bundles all required files together for easy export to Raijin

--- savemagics.py
+++ savemagics.py
@@ -9,6 +9,9 @@ Created on Thu Nov 30 11:06:15 2017
 from IPython.core.magic import (Magics, magics_class, line_magic, cell_magic)
 import time
 import getpass
+import os
+import shutil
+
 
 
 @magics_class
@@ -60,30 +63,65 @@ def name():\n\
     
 get_ipython().register_magics(CellWriter)
 
+def package(name, nwalkers, ntemps, nsteps, nthin, nCPUs=8,
+            vfp_location='/mnt/1D9D9A242359B87C/Git Repos/refnx/examples/\
+analytical_profiles/brushes/brush.py'):
+    
+    direc = name + '/'    
+    objective_file = name + '.py'
+    data_file = name + '.dat'
+    
+    file = open(objective_file, 'r')
+    exec(file.read(), globals())
+    objective = setup(data_file)
+        
+    nparameters = len(objective.varying_parameters())
+    print (nparameters)
+    
+    if not os.path.exists(name):
+        os.makedirs(name)
+    
+    
+    writeMPI(name, direc, nwalkers, ntemps, nsteps, nthin)
+    
+    walltime = approx_walltime(nwalkers, ntemps, nsteps, nparameters, nCPUs)
+    writeShell(name, direc, walltime, nCPUs)
+    
+    os.rename(objective_file, direc + objective_file)
+    shutil.copyfile(data_file, direc + data_file)
+    shutil.copyfile(vfp_location, name + '/brush.py')
+    
 
-def writeMPI(objective_name, nwalkers, ntemps, nsteps, nthin, init_method = 'prior', buffering=1000):
+
+def approx_walltime(nwalkers, ntemps, nsteps, nparameters, nCPUs, time_per_calc = 0.005):
+    calcs_per_CPU = nwalkers * ntemps * nsteps * nparameters / nCPUs
+    time_per_calc = 0.005
+    time_min = calcs_per_CPU * time_per_calc / 60
+    return "%02d:%02d:00" % (time_min/60, time_min%60)
+    
+def writeMPI(objective_name, direc, nwalkers, ntemps, nsteps, nthin, init_method = 'prior', buffering=100):
     """
     """
     
     code = "\
 import sys\n\
 from refnx.analysis import CurveFitter\n\
-from emcee.utils import MPIPool\n\
+from emcee import interruptible_pool\n\
 from %s import *\n\
 objective = setup('%s.dat')\n\
-with MPIPool() as pool:\n\
-    if not pool.is_master():\n\
-        pool.wait()\n\
-        sys.exit(0)\n\
+with interruptible_pool.InterruptiblePool() as pool:\n\
+    #if not pool.is_master():\n\
+    #    pool.wait()\n\
+    #    sys.exit(0)\n\
 \n\
     nwalkers=%d\n\
     ntemps=%d\n\
     nsteps=%d\n\
     nthin=%d\n\
 \n\
-    fitter_dry = CurveFitter(objective, nwalkers=nwalkers, ntemps=ntemps)\n\
+    fitter = CurveFitter(objective, nwalkers=nwalkers, ntemps=ntemps)\n\
 \n\
-    fitter_dry.initialise('%s')\n\
+    fitter.initialise('%s')\n\
 \n\
     with open('%s' + '_samplechain_' +\n\
               str(nwalkers) + 'walkers_' +\n\
@@ -92,32 +130,32 @@ with MPIPool() as pool:\n\
               str(nthin) + 'thinned.txt',\n\
               'a', buffering=%d) as fh:\n\
 \n\
-        fitter_dry.sample(nsteps, nthin=nthin, pool=pool, verbose=True, f=fh)\
+        fitter.sample(nsteps, nthin=nthin, pool=pool, verbose=True, f=fh)\
 "%(objective_name, objective_name, nwalkers, ntemps, nsteps, nthin, init_method, objective_name, buffering)
         
-    filename = objective_name + "_run.py"
+    filename = direc + objective_name + "_run.py"
         
     with open(filename, 'w') as fh: # Save the cell + metadata as a .py file
         fh.write(code)
 
 
-def writeShell(name, timeh):
+def writeShell(name, direc, timeh, nCPUs):
     script = "\
 #!/bin/bash\n\
 #PBS -P rr87\n\
 #PBS -q normal\n\
-#PBS -l walltime=%d:00:00\n\
+#PBS -l walltime=%s\n\
 #PBS -l mem=2GB\n\
 #PBS -l jobfs=2GB\n\
-#PBS -l ncpus=8\n\
+#PBS -l ncpus=%d\n\
 #PBS -l software=refnx\n\
 #PBS -l wd\n\
 \n\
 source home/561/ig8882/venv/refnx-activate\n\
 \n\
-mpirun -np 8 python %s_run.py"%(timeh, name)
+mpirun -np 8 python %s_run.py"%(timeh, nCPUs, name)
 
-    filename = name + ".sh"
+    filename = direc + name + ".sh"
         
     with open(filename, 'w') as fh: # Save the cell + metadata as a .py file
         fh.write(script)
