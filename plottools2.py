0f741b4f47812f4f0e58eea7f10783c3662aefba
Isaac Gresham <igresham27@gmail.com>
2020-09-03 12:17:44 +1000

    minor changes + completion of docstrings

--- plottools2.py
+++ plottools2.py
@@ -27,20 +27,19 @@ def graph_plot(report=None, objective=None, sld_plot=True, refl_plot=True,
     objective : refnx.objective
         Objective object to be plotted. Will just convert this to a report
         before plotting
-    sld_plot : TYPE, optional
-        DESCRIPTION. The default is True.
-    refl_plot : TYPE, optional
-        DESCRIPTION. The default is True.
-    vf_plot : TYPE, optional
-        DESCRIPTION. The default is False.
-    plot_knots : TYPE, optional
-        DESCRIPTION. The default is False.
+    sld_plot : bool, optional
+        If true will plot an SLD profile. The default is True.
+    refl_plot : bool, optional
+        If true will plot an reflectonmetry profile. The default is True.
+    vf_plot : bool, optional
+        If true will plot an SLD profile - note, there must be a element in the
+        structure with a volume fraction component. The default is False.
     fig : matplotlib figure
         graph_plot will use supplied fig object if provided. If None will
-        generate its own fig and ax objects
+        generate its own fig and ax objects. The default is None.
     ax : matplotlib axis
         graph_plot will use supplied axis object if provided. If None will
-        generate its own fig and ax objects
+        generate its own fig and ax objects. The Default is None.
     logpost_limits : list)
         List containing lower and upper limits of the logpost for the system.
         If provided will set the colour of profiles based on their probability
@@ -109,52 +108,50 @@ def graph_plot(report=None, objective=None, sld_plot=True, refl_plot=True,
             colors = [color] * num_reports
 
         for rep, col in zip(report, colors):
-            _report_graph_plot(rep, plot_knots=False, fig=fig, ax=ax,
+            _report_graph_plot(rep, plot_knots=False, ax=ax,
                                logpost_limits=logpost_limits, ystyle=ystyle,
                                color=col, cbar=cbar, offset=reflOS)
             reflOS *= offset
     else:
-        _report_graph_plot(report, plot_knots=False, fig=fig, ax=ax,
+        _report_graph_plot(report, plot_knots=False, ax=ax,
                            logpost_limits=logpost_limits, ystyle=ystyle,
                            color=color, cbar=cbar)
 
     return fig, ax
 
 
-def _report_graph_plot(report, plot_knots=False, fig=None, ax=None,
-                       logpost_limits='auto', ystyle='log', xstyle='lin',
-                       color=None, cbar=False, offset=1):
+def _report_graph_plot(report, ax, logpost_limits='auto', ystyle='r',
+                       xstyle='lin', color=None, cbar=False, offset=1):
     """
-    
+    Plot a single report on a given set of axes.
 
     Parameters
     ----------
-    report : TYPE
-        DESCRIPTION.
-    plot_knots : TYPE, optional
-        DESCRIPTION. The default is False.
-    fig : TYPE, optional
-        DESCRIPTION. The default is None.
-    ax : TYPE, optional
-        DESCRIPTION. The default is None.
+    report : objective_processing.objective_report
+        report to plot.
+    ax : matplotlib axis
+        axis to plot on.
     logpost_limits : TYPE, optional
         DESCRIPTION. The default is 'auto'.
-    ystyle : TYPE, optional
-        DESCRIPTION. The default is 'log'.
-    xstyle : TYPE, optional
-        DESCRIPTION. The default is 'lin'.
-    color : TYPE, optional
-        DESCRIPTION. The default is None.
-    cbar : TYPE, optional
-        DESCRIPTION. The default is False.
-    offset : TYPE, optional
-        DESCRIPTION. The default is 1.
-
-    Returns
-    -------
-    fig : TYPE
-        DESCRIPTION.
-
+    ystyle : string, optional
+        Style of the reflectometry y-axis. Options are 'r' for normal
+        log-scaling, 'rq2' for q^2 scaling, and 'rq4' for rq4 scaling.
+        The default is 'r'.
+    xstyle : string, optional
+        Style of the reflectometry x-axis. Options and 'lin' for a linear
+        scale and 'log' for a log scale. The default is 'lin'.
+    color : any color format understood by matploblib, or matplotlib.pyplot.cm
+        Color to use. If a colurmap is provided plots will be coloured
+        according to their probability. The default is None, in which case
+        profiles will be black.
+    cbar : bool, optional
+        Wheter or not to include a colorbar. A colormap must have been provided
+        as the color. The default is False.
+    offset : float, optional
+        Value to allow vertical offset of reflectometry data, for use when
+        multiple reports or objectives are supplied if you want to vertically
+        offset curves a value of 0.01 is reccomended. The default is 1, which
+        does not offset curves.
     """
     vfps = report.model.vfp
     slds = report.model.sld
@@ -188,30 +185,65 @@ def _report_graph_plot(report, plot_knots=False, fig=None, ax=None,
         plot_profiles(slds, ax=axSLD, line_plotter=lp, cmap_keys=logposts)
     if axR:
         plot_profiles(refs, ax=axR, line_plotter=lp, cmap_keys=logposts,
-                      ymult=ymult, offset=offset)
+                      ymult=ymult * offset)
 
     if cbar:
         lp.make_cbar(axR)
 
-    return fig
 
-def plot_profiles(profiles, ax, line_plotter, cmap_keys, offset=1, ymult=1):
+def plot_profiles(profiles, ax, line_plotter, cmap_keys, ymult=1):
+    """
+    Iterate through provided profiles and plot them using lineplotter.
+
+    Parameters
+    ----------
+    profiles : list or array of shape n, 2, z.
+         n is the number of profiles, and z is the number of points in each
+         profile.
+    ax : matplotlib axis
+        axis to plot on.
+    line_plotter : plottools.lineplotter
+        class that handles styling and plotting of lines.
+    cmap_keys : list or array of shape n
+        will be used to set the color of each line in the profile, if a
+        colormap is being used.
+    ymult : float, optional
+        Vertical offset for the profile (y datapoints will be multiplied by
+        this value). The default is 1.
+
+    """
     for profile, cmap_key in zip(profiles, cmap_keys):
         line_plotter.plot_line(ax,
                                profile[0],
-                               profile[1] * offset * ymult,
+                               profile[1] * ymult,
                                cmap_key=cmap_key)
 
 
 class lineplotter (object):
-    def __init__(self, weight=2, color='k', alpha=1, cmap_bounds=[0,1]):
+    """
+    Handles plotted linestyles.
+
+    Parameters
+    ----------
+    weight : float, optional
+        Line weight. The default is 2.
+    color : any color format understood by matploblib, or matplotlib.pyplot.cm
+        The color the lines will be. The default is 'k'.
+    alpha : float, optional
+        The alpha value of the line. The default is 1.
+    cmap_bounds : list or tuple, optional
+        bounds to use for the colormap. Will depend on what is being used for
+        the colormap key. The default is [0, 1].
+    """
+
+    def __init__(self, weight=2, color='k', alpha=1, cmap_bounds=[0, 1]):
         self.lw = weight
 
         if color is None:
             self._col = 'k'
             self._colormap = False
         elif 'colormap' in str(color).lower():
-            norm = clrs.Normalize(vmin=cmap_bounds[0],vmax=cmap_bounds[1])
+            norm = clrs.Normalize(vmin=cmap_bounds[0], vmax=cmap_bounds[1])
             self._col = plt.cm.ScalarMappable(cmap=color, norm=norm)
             self._colormap = True
         else:
@@ -223,6 +255,14 @@ class lineplotter (object):
         self._cmap_key = None
 
     def make_cbar(self, ax):
+        """
+        Make a colorbar on a given axis.
+
+        Parameters
+        ----------
+        ax : matplotplib axis
+            The axis to plot the colorbar on.
+        """
         if self._colormap:
             plt.colorbar(self._col, ax=ax)
         else:
@@ -240,12 +280,36 @@ class lineplotter (object):
 
     @property
     def get_color(self):
+        """
+        Sanitises self._col, returning a single color.
+
+        Returns
+        -------
+        a color format understood by matplotlib
+        """
         if self._colormap:
             return self._col.to_rgba(self._cmap_key)
         else:
             return self._col
 
     def plot_line(self, ax, x, y, cmap_key=None, **kwargs):
+        """
+        Plot a line with characteristics defined by the class.
+
+        Parameters
+        ----------
+        ax : matplotplib axis
+            The axis to plot the line on.
+        x : list, np.array
+            horizontal axis datapoints.
+        y : list, np.array
+            vertical axis datapoints.
+        cmap_key : float, optional
+            If a color map is being used cmap_key is used to determine the
+            color of the line. The default is None.
+        **kwargs : dict
+            Supplies to matplotlib.pyplot.plot. Overrides class settings.
+        """
         self._cmap_key = cmap_key
         mkwargs = self._merge_kwargs_dicts(kwargs)
         ax.plot(x, y, **mkwargs)
@@ -254,22 +318,58 @@ class lineplotter (object):
 def CreateAxes(fig=None, sld_plot=True, refl_plot=True, vf_plot=False,
                orientation='h', ystyle='r', xstyle='lin',
                fig_kwargs=None):
+    """
+    Create figure and axis objects.
 
+    Parameters
+    ----------
+    fig : TYPE, optional
+        DESCRIPTION. The default is None.
+    sld_plot : TYPE, optional
+        DESCRIPTION. The default is True.
+    refl_plot : TYPE, optional
+        DESCRIPTION. The default is True.
+    vf_plot : TYPE, optional
+        DESCRIPTION. The default is False.
+    orientation : TYPE, optional
+        DESCRIPTION. The default is 'h'.
+    ystyle : TYPE, optional
+        DESCRIPTION. The default is 'r'.
+    xstyle : TYPE, optional
+        DESCRIPTION. The default is 'lin'.
+    fig_kwargs : TYPE, optional
+        DESCRIPTION. The default is None.
+
+    Raises
+    ------
+    ValueError
+        DESCRIPTION.
+
+    Returns
+    -------
+    fig : TYPE
+        DESCRIPTION.
+    to_plot_ls : TYPE
+        DESCRIPTION.
+
+    """
     to_plot_ls = [vf_plot, sld_plot, refl_plot]
 
-    if type(sld_plot) is not bool or type(refl_plot) is not bool or type(vf_plot) is not bool:
-        assert sld_plot is not True
-        assert refl_plot is not True
-        assert vf_plot is not True
+    if type(sld_plot) is not bool or \
+       type(refl_plot) is not bool or\
+       type(vf_plot) is not bool:
+        assert sld_plot is not True, "Unsure what to do with bool and axis"
+        assert refl_plot is not True, "Unsure what to do with bool and axis"
+        assert vf_plot is not True, "Unsure what to do with bool and axis"
 
     else:
         num_axes = sld_plot + refl_plot + vf_plot
         if fig_kwargs is None:
-            fig_kwargs = {'figsize': (1 + 3*num_axes, 3), 'dpi': 100,
+            fig_kwargs = {'figsize': (1 + 3 * num_axes, 3), 'dpi': 100,
                           'constrained_layout': True}
         assert num_axes != 0
         if orientation.lower() == 'v':
-            fig_kwargs['figsize'] = (4, 1 + 2*num_axes)
+            fig_kwargs['figsize'] = (4, 1 + 2 * num_axes)
             fig, AXS = plt.subplots(num_axes, 1, **fig_kwargs)
         elif orientation.lower() == 'h':
             fig, AXS = plt.subplots(1, num_axes, **fig_kwargs)
